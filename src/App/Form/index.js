4fd1ea3a3cfb18d808a3d1b07d8c1c6475ab1932
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-03-11 17:18:46 +0100

    "import React" deleted

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./style.css";
 import { Result } from "./Result";
 import { currencies } from "../currencies.js";
--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Form } from "./Form";
 import "./App.css";
 import { currencies } from "./currencies.js";
