65cf0efff0b778fe522d732d7edb6a231cf3f3d2
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-03-11 20:48:41 +0100

    useCurrentDate hook added

--- src/App/Form/Time/index.js
+++ src/App/Form/Time/index.js
@@ -1,29 +1,19 @@
 import { Content } from "./styled";
-import { useState, useEffect } from "react";
+import { useCurrentDate } from "./useCurrentDate";
+
+const timeString = (time) =>
+time.toLocaleString("PL", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
+});
 
 export const Time = () => {
-    const [time, setTime] = useState(new Date());
-
-    const timeString = (time) =>
-        time.toLocaleString("PL", {
-            weekday: "long",
-            day: "numeric",
-            month: "long",
-            year: "numeric",
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric"
-        });
-
-    useEffect(() => {
-        const intervalID = setInterval(() => {
-            setTime(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalID);
-        };
-    }, []);
+const time = useCurrentDate();
 
     return (
         <Content>
---
+++ src/App/Form/Time/useCurrentDate.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const useCurrentDate = () => {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalID);
+        };
+    }, []);
+
+    return time
+};
