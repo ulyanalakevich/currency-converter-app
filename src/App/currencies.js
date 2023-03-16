df7c0cdb3aecae3887103ee9d010725cdb382a0b
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-03-16 21:23:18 +0100

    useRatesData hook added

--- src/App/currencies.js
+++ src/App/currencies.js
@@ -1,14 +1,32 @@
-export const currencies = [
-    {
-        name: "USD",
-        rate: 4.43,
-    },
-    {
-        name: "EUR",
-        rate: 4.71,
-    },
-    {
-        name: "PLN",
-        rate: 1.00,
-    }
-];
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export const useRatesData = () => {
+    const [ratesData, setRatesData] = useState({
+        state: "loading",
+    });
+
+    useEffect = (() => {
+        const getData = async () => {
+            try {
+                const response = await axios.get("https://api.exchangerate.host/latest?base=PLN");
+                const { rates, date } = await response.json();
+
+                setRatesData({
+                    state: "success",
+                    rates,
+                    date,
+                })
+
+            } catch (error) {
+                setRatesData({
+                    state: "error",
+                });
+            };
+        };
+        setTimeout(getData, 1000);
+    }, []);
+
+return ratesData;
+};
+
